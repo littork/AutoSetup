71b38dc6d82b937e14b61ebc7a8969fac7dff25a
Littork <littork@gmail.com>
2017-09-02 17:35:46 -0600

    Added prompts

--- Linux/Ubuntu/ubuntuSetup_16.04_MainPC.sh
+++ Linux/Ubuntu/ubuntuSetup_16.04_MainPC.sh
@@ -7,37 +7,93 @@ sudo add-apt-repository ppa:lah7/polychromatic -y
 sudo rm /etc/apt/sources.list.d/google-chrome*
 sudo apt-get update -y
 
-sudo apt-get install -y ubuntu-gnome-desktop
+read -p "Install ubuntu-gnome-desktop? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo apt-get install -y ubuntu-gnome-desktop
+fi
 
 sudo apt-get remove -y --auto-remove account-plugin-aim account-plugin-facebook account-plugin-flickr account-plugin-jabber account-plugin-salut account-plugin-yahoo aisleriot gnome-mahjongg gnome-mines gnome-sudoku landscape-client-ui-install unity-lens-music unity-lens-photos unity-lens-video firefox unity-webapps-common evolution empathy rhythmbox libreoffice* cheese activity-log-manager brasero gnome-calendar checkbox-* gnome-contacts gnome-documents gnome-maps gnome-music onboard gnome-online-accounts gnome-photos remmina shotwell simple-scan gnome-orca thunderbird transmission* evince eog seahorse gnome-screenshot gnome-weather totem xdiagnose webbrowser-app
 
 sudo rm google-chrome*.deb
-sudo apt-get install -y libxss1 libappindicator1 libindicator7
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*.deb
-sudo rm google-chrome*.deb
+read -p "Install google chrome? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo apt-get install -y libxss1 libappindicator1 libindicator7
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+	sudo dpkg -i google-chrome*.deb
+	sudo rm google-chrome*.deb
+fi
 
 sudo rm tixati*.deb
-wget https://download2.tixati.com/download/tixati_2.55-1_amd64.deb
-sudo dpkg -i tixati*.deb
-sudo rm tixati*.deb
+read -p "Install tixati? (A torrent client) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	wget https://download2.tixati.com/download/tixati_2.55-1_amd64.deb
+	sudo dpkg -i tixati*.deb
+	sudo rm tixati*.deb
+fi
 
 sudo rm atom.deb
-wget -O atom.deb https://atom.io/download/deb
-sudo dpkg -i atom.deb
-sudo rm atom.deb
+read -p "Install atom? (A great text editor) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	wget -O atom.deb https://atom.io/download/deb
+	sudo dpkg -i atom.deb
+	sudo rm atom.deb
+fi
 
-sudo apt-get install -y python3-razer razer-kernel-modules-dkms razer-daemon razer-doc
-sudo apt-get install -y polychromatic
-sudo apt-get install -y macchanger
-sudo apt-get install -y steam
-sudo apt-get install -y libgles1-mesa libglapi-mesa vlc
-sudo apt-get install -y sublime-text-installer
-sudo apt-get install -y virtualbox virtualbox-ext-pack
+read -p "Install Razer RGB product support? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo apt-get install -y python3-razer razer-kernel-modules-dkms razer-daemon razer-doc
+	sudo apt-get install -y polychromatic
+fi
+
+read -p "Install MacChanger? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo apt-get install -y macchanger
+fi
+
+read -p "Install steam? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo apt-get install -y steam
+fi
+
+read -p "Install Virtualbox + Extension Pack? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo apt-get install -y virtualbox virtualbox-ext-pack
+fi
+
 sudo apt-get install -y git
-sudo apt-get install -y love
+
+read -p "Install Love2D? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo apt-get install -y love
+fi
+
 sudo apt-get install -y htop
-sudo apt-get install -y lua5.2
+
+read -p "Install LUA 5.2? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo apt-get install -y lua5.2
+fi
+
 sudo apt-get install -y gparted
 sudo apt-get install -y fail2ban
 sudo apt-get install -y ssh
@@ -51,3 +107,5 @@ sudo apt-get install -y -f
 sudo apt-get upgrade -y
 
 sudo apt-get autoremove -y
+
+echo "It would be in your best interests to reboot now"
